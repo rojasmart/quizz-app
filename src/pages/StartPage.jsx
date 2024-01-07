2607cf2eb2956910b22e61df1ddcf2346df75e40
binarypsilocybin <rogeriosvaldo@gmail.com>
2024-01-07 16:17:53 +0000

    styled first page cards

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -6,9 +6,15 @@ import {
   Text,
   Button,
   VStack,
+  HStack,
   Image,
   Box,
 } from "@chakra-ui/react";
+
+import htmlIcon from "../assets/images/icon-html.svg";
+import cssIcon from "../assets/images/icon-css.svg";
+import javascriptIcon from "../assets/images/icon-js.svg";
+import accessibilityIcon from "../assets/images/icon-accessibility.svg";
 
 export default function StartPage() {
   return (
@@ -26,15 +32,54 @@ export default function StartPage() {
         </Container>
         <Container maxW="lg">
           <VStack>
-            <Card>
+            <Card width="lg" rounded="3xl">
               <CardBody>
-                <Text>HTML</Text>
+                <HStack spacing="4">
+                  <Box bg="#FFF1E9" p={2} borderRadius="lg">
+                    <Image src={htmlIcon} alt="icon-html" />
+                  </Box>
+                  <Text fontSize="lg" as="b">
+                    HTML
+                  </Text>
+                </HStack>
               </CardBody>
             </Card>
-            <Button size="lg">HTML</Button>
-            <Button size="lg">CSS</Button>
-            <Button size="lg">Javascript</Button>
-            <Button size="lg">Accessibility</Button>
+            <Card width="lg" rounded="3xl">
+              <CardBody>
+                <HStack spacing="4">
+                  <Box bg="#E0FDEF" p={2} borderRadius="lg">
+                    <Image src={cssIcon} alt="icon-html" />
+                  </Box>
+                  <Text fontSize="lg" as="b">
+                    CSS
+                  </Text>
+                </HStack>
+              </CardBody>
+            </Card>
+            <Card width="lg" rounded="3xl">
+              <CardBody>
+                <HStack spacing="4">
+                  <Box bg="#EBF0FF" p={2} borderRadius="lg">
+                    <Image src={javascriptIcon} alt="icon-html" />
+                  </Box>
+                  <Text fontSize="lg" as="b">
+                    Javascript
+                  </Text>
+                </HStack>
+              </CardBody>
+            </Card>
+            <Card width="lg" rounded="3xl">
+              <CardBody>
+                <HStack spacing="4">
+                  <Box bg="#F6E7FF" p={2} borderRadius="lg">
+                    <Image src={accessibilityIcon} alt="icon-html" />
+                  </Box>
+                  <Text fontSize="lg" as="b">
+                    Accessibility
+                  </Text>
+                </HStack>
+              </CardBody>
+            </Card>
           </VStack>
         </Container>
       </Stack>
